146f15a4e7ff4b7ed9cdd280aaeaf55c522a59f6
Shakthi Kumar <shakthikumar@zencloudtech.com>
2018-03-23 18:09:39 +0530

    Added functionality to Blog and Gallery buttons to hide and show them.

--- MyPortfolioPage with JS and Jquey/js.js
+++ MyPortfolioPage with JS and Jquey/js.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+$('.gallery').hide();
+
 $('aside legend').on('click', function(){
 	$('aside * li').slideToggle(1000);
 
@@ -10,7 +12,19 @@ $('aside * li').on('click', ".fa", function(){
 	$(this).closest('li').find('.noEdit').attr('contentEditable', 'false');
 });
 
+$('.GalleryButton').click(function(){
+	$('.right').hide();
+	$('.gallery').show();
+});
 
+$('#BlogButton').click(function(){
+	$('.right').show();
+	$('.gallery').hide();
+});
+
+// .ma-xs { margin: 5px }
+// .ma-s { margin: 10px }
+// .ma-m { margin: 15px }
 
 
 
